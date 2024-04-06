a409d7f222c108bcc77ca46e4d7d6f2b1e70a838
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2024-04-06 17:09:52 -0300

    test: Add test scenes

---
+++ tests/scenes/levels/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+
+signal died
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta: float) -> void:
+	var is_movement_enabled: bool = FDCore.get_project_manager().is_player_movement_enabled()
+	if not is_movement_enabled:
+		return
+
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+	# Player death
+	if position.y > 700:
+		died.emit()
+
