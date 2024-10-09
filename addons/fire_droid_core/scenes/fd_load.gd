9e422bd4427ff69924461b503ca52cb2a693b654
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2024-10-09 15:16:52 -0300

    feat: Implement FDLoadBatch (subclass of FDLoad)

--- addons/fire_droid_core/scenes/fd_load.gd
+++ addons/fire_droid_core/scenes/fd_load.gd
@@ -1,7 +1,11 @@
 extends Node
 
 
-var abort_on_failure: bool = false
+var default_retry_limit: int = 1
+var default_cache_mode: ResourceLoader.CacheMode = (
+	ResourceLoader.CacheMode.CACHE_MODE_REUSE
+)
+var batch_size: int = 10
 
 
 func _ready() -> void:
@@ -16,19 +20,24 @@ func _physics_process(_delta: float) -> void:
 	pass
 
 
+func get_requests_count() -> int:
+	return 0
+
+
 class FDLoadRequest extends Node:
 	signal finished()
 	signal failed()
+	signal progress_changed(previous_progress: int, current_progress: int)
 
 
 	var path: String = ""
 	var use_subthread: bool = true
-	var cache_mode: ResourceLoader.CacheMode = (
-		ResourceLoader.CacheMode.CACHE_MODE_REUSE
-	)
 	var type_hint: String = ""
-	var retry_limit: int = 1
-	var _progress: Array = [0.0]
+	var retry_limit: int = FDLoad.default_retry_limit
+	var cache_mode: ResourceLoader.CacheMode = FDLoad.default_cache_mode
+	var _current_progress: int = 0
+	var _progress_array: Array = [0.0]
+	var _previous_progress: int = 0
 
 
 	func _ready() -> void:
@@ -37,18 +46,21 @@ class FDLoadRequest extends Node:
 
 
 	func _process(_delta: float) -> void:
+		if not _previous_progress == _current_progress:
+			progress_changed.emit(_previous_progress, _current_progress)
 		if is_loaded():
 			finished.emit()
 			set_process(false)
 
 
 	func get_progress() -> float:
-		return _progress[0]
+		return _progress_array[0]
 
 
 	func start_load() -> void:
 		if path.is_empty():
-			_progress[0] = 1.0
+			_progress_array[0] = 1.0
+			_current_progress = 100
 			finished.emit()
 			return
 		elif is_loaded():
@@ -66,9 +78,11 @@ class FDLoadRequest extends Node:
 
 
 	func is_loaded() -> bool:
+		_previous_progress = _current_progress
 		var load_status: int = (
-			ResourceLoader.load_threaded_get_status(path, _progress)
+			ResourceLoader.load_threaded_get_status(path, _progress_array)
 		)
+		_current_progress = int(_progress_array[0] * 100)
 		return (
 			load_status == ResourceLoader.ThreadLoadStatus.THREAD_LOAD_LOADED
 		)
@@ -82,3 +96,61 @@ class FDLoadRequest extends Node:
 			set_process(true)
 			return true
 		return false
+
+
+class FDLoadBatch extends Node:
+	signal finished()
+	signal failed()
+	signal progress_sum_changed(progress_sum: int)
+
+
+	var _requests: Array[FDLoadRequest] = []
+	var _is_aborted: bool = false
+	var _has_started: bool = false
+	var _progress_sum: int = 0
+	var _finished_count: int = 0
+
+
+	func _init(batch_index: int, load_queue: Array[FDLoadRequest]) -> void:
+		_requests = load_queue.slice(batch_index, FDLoad.batch_size)
+
+
+	func _ready() -> void:
+		set_process(false)
+		set_physics_process(false)
+		for request in _requests:
+			add_child(request)
+			request.finished.connect(_on_request_finished)
+			request.progress_changed.connect(_on_request_progress_changed)
+
+
+	func abort() -> void:
+		_is_aborted = true
+
+
+	func get_progress_sum() -> int:
+		return _progress_sum
+
+
+	func start_load() -> void:
+		if _is_aborted:
+			return
+		for request in _requests:
+			request.start_load()
+
+
+	func has_finished() -> bool:
+		return _finished_count == _requests.size()
+
+
+	func _on_request_finished() -> void:
+		_finished_count += 1
+		if _finished_count == _requests.size():
+			finished.emit()
+
+
+	func _on_request_progress_changed(
+		previous_progress: float, current_progress: float
+	) -> void:
+		_progress_sum += current_progress - previous_progress
+		progress_sum_changed.emit(_progress_sum)
