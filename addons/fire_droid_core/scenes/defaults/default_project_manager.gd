78917705d3acd6abdc93eaf97fea8173ed4f2d41
Lucas Queiroz <35619327+Firemanarg@users.noreply.github.com>
2024-10-15 21:19:51 -0300

    fix: Fix default main scene path

--- addons/fire_droid_core/scenes/defaults/default_project_manager.gd
+++ addons/fire_droid_core/scenes/defaults/default_project_manager.gd
@@ -2,7 +2,7 @@ extends FDProjectManager
 
 
 func _init() -> void:
-	initial_scene = preload("res://addons/fire_droid_core/main.tscn")
+	initial_scene = preload("res://addons/fire_droid_core/scenes/defaults/main.tscn")
 
 
 func _on_action_triggered(action: StringName, context: StringName = &"") -> void:
